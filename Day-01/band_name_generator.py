0b8909dbff7e90858d7b0727232362f2bf0a67e0
Vidyavrat Taak <104916033+Vidyavrat1999@users.noreply.github.com>
2025-01-25 17:18:15 +0000

    completed code

--- Day-01/band_name_generator.py
+++ Day-01/band_name_generator.py
@@ -2,3 +2,8 @@ print("WELCOME TO BAND NAME GENERATOR")
 
 # Ask for city name
 city = input("Which city did you grew up in?")
+
+# Ask for pet name
+pet = input("What is your pet's name?")
+
+print("Your band name could be: " + city + " " + pet + ".")
